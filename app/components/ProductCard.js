47e08fd6cde878965182f21423ddfccec0cef7aa
Sabir Hussain <sabirhussain@Sabirs-MBP.lan>
2023-09-28 17:07:01 +0100

    fix: console log

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import React from 'react'
 
 function ProductCard({product}) {
-    console.log(product);
     const { name, metadata } = product;
 
     const pricingURL = `/pricing/${metadata.slug}`
