17b95d1ca4e2a076e86887265847c13a5e62b045
Sabir Hussain <isabir.dev@gmail.com>
2024-09-05 15:20:10 +0100

    route upadte

--- app/api/payment/route.js
+++ app/api/payment/route.js
@@ -1,31 +1,36 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
 
-import Stripe from 'stripe';
-import { NextResponse } from 'next/server';
-
-export async function POST (request) {
+export async function POST(request) {
+  try {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     let data = await request.json();
 
-
     const session = await stripe.checkout.sessions.create({
-    mode: 'subscription',
-    line_items: [
+      mode: "subscription",
+      line_items: [
         {
-        price: data.priceId,
-        // For metered billing, do not pass quantity
-        quantity: 1,
+          price: data.priceId,
+          // For metered billing, do not pass quantity
+          quantity: 1,
         },
-    ],
-    // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
-    // the actual Session ID is returned in the query parameter when your customer
-    // is redirected to the success page.
-    success_url: process.env.BASE_URL + '/thankyou?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url: process.env.BASE_URL,
-    // success_url: 'https://localhost:3000/success.html?session_id={CHECKOUT_SESSION_ID}',
-    // cancel_url: 'https://localhost:3000/canceled.html',
+      ],
+      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+      // the actual Session ID is returned in the query parameter when your customer
+      // is redirected to the success page.
+      success_url:
+        process.env.BASE_URL + "/thankyou?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: process.env.BASE_URL,
+      // success_url: 'https://localhost:3000/success.html?session_id={CHECKOUT_SESSION_ID}',
+      // cancel_url: 'https://localhost:3000/canceled.html',
     });
 
     // return NextResponse.redirect(session.url, { status: 303 });
     return NextResponse.json(session.url);
-
+  } catch (error) {
+    console.error("Error:", error.message);
+    return new Response("Internal Server Error", {
+      status: 500,
+    });
+  }
 }
